e484ac5c89281c3725fb2d573264de7904b0dbe6
hicelery <mungoharrison.ward9@gmail.com>
2026-01-29 14:58:02 +0000

    posts model

--- env.py
+++ env.py
@@ -2,4 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL","postgresql://neondb_owner:npg_n1GmdApZbsx7@ep-sweet-hall-agk4ci9n.c-2.eu-central-1.aws.neon.tech/icon_spool_stool_479188")
-os.environ.setdefault("SECRET_KEY", "15.5g1g$-FUM2:&ap9XBcl(h*]CvYkB.")
+os.environ.setdefault(
+    "SECRET_KEY", "15.5g1g$-FUM2:&ap9XBcl(h*]CvYkB.")
