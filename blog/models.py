45cb2c347b5e6e13c07bb6ff5cf35dfd8b57718e
hicelery <mungoharrison.ward9@gmail.com>
2026-01-30 18:50:16 +0000

    updated field

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User # Import models to connect
+from django.contrib.auth.models import User  # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -12,5 +14,6 @@ class Post(models.Model):
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
